55123fe4361564d3180a5a7e567ddaf25582fdff
test <1639250983@qq.com>
2017-07-06 14:01:07 +0800

    Finish layout and routes

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,14 @@
+<div class="col-md-12">
+    <footer class="footer">
+        <small class="slogon">
+            <a href="http://estgroupe.com/">
+                来吧，互相伤害吧
+            </a>
+        </small>
+        <nav>
+            <ul>
+                <li><a href="/about">关于</a></li>
+            </ul>
+        </nav>
+    </footer>
+</div>
